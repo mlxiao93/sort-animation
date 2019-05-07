b7c7abf579bcc9d98f6def077a08e47997ffb4ac
xiaomaolin <xiaomaolin@meituan.com>
2019-05-07 22:41:54 +0800

    modify public path

---
+++ docs/precache-manifest.932d5b1f1cbb6db36d251b01e940b9ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5223f8f608779d785507699c787b3ff3",
+    "url": "/sort-animation/index.html"
+  },
+  {
+    "revision": "8ed3ac401febfc4ac785",
+    "url": "/sort-animation/static/css/main.a8c545f8.chunk.css"
+  },
+  {
+    "revision": "056a0ed83ad4c5ee0700",
+    "url": "/sort-animation/static/js/2.af0c708e.chunk.js"
+  },
+  {
+    "revision": "8ed3ac401febfc4ac785",
+    "url": "/sort-animation/static/js/main.5e88d931.chunk.js"
+  },
+  {
+    "revision": "636969b7fbbb3ed207dc",
+    "url": "/sort-animation/static/js/runtime~main.7f407b46.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/sort-animation/docs/precache-manifest.981ee6fdd8dd633151cf75df227600a8.js"
+  "/sort-animation/precache-manifest.932d5b1f1cbb6db36d251b01e940b9ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sort-animation/docs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sort-animation/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
